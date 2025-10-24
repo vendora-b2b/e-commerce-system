709a99d6fb3dea395b355a658d18cc093b2416cb
maixuanbach174 <22125005@student.hcmus.edu.vn>
2025-10-24 08:38:20 +0700

    HOTFIX database for test build

--- src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class EcommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
 	}
 
 }
